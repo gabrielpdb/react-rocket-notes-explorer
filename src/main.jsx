5b0b0de42058180a33f2a74263b9c231cbb5cb5b
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2022-06-13 21:37:52 -0300

    SignUp

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,13 @@ import theme from './styles/theme'
 import { Details } from './pages/Details'
 import { Home } from './pages/Home'
 import { Signin } from './pages/Signin'
+import { Signup } from './pages/Signup'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Signin />
+      <Signup />
     </ThemeProvider>
   </React.StrictMode>
 )
