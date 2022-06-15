952fe78794f1f6db30a76a6efe8f656bf8daf3b7
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2022-06-14 21:15:33 -0300

    Avatar do usuário

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,5 @@
-import { Container, Form } from './styles'
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi'
+import { Container, Form, Avatar } from './styles'
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
@@ -13,6 +13,13 @@ export function Profile() {
         </a>
       </header>
       <Form>
+        <Avatar>
+          <img src="https://github.com/gabrielpdb.png" alt="Foto do usuário" />
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
         <Input placeholder="Nome" type="text" icon={FiUser} />
         <Input placeholder="Email" type="text" icon={FiMail} />
         <Input placeholder="Senha atual" type="password" icon={FiLock} />
