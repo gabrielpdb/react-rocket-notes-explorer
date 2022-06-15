0352424281ab547ab81b93ab491aa8f3d0b33de6
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2022-06-14 21:56:55 -0300

    Finalizando o New

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -3,6 +3,7 @@ import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
 import { NoteItem } from '../../components/NoteItem'
 import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
 
 import { Container, Form } from './styles'
 
@@ -22,6 +23,14 @@ export function New() {
             <NoteItem value="https://rocketseat.com.br" />
             <NoteItem value="" isNew placeholder="Novo link" />
           </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="react" />
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
