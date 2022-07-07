fce0ff98e578e26434d6fc2f171c17b7a62c907e
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2022-07-07 06:35:17 -0300

    fix in routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,7 +5,7 @@ import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
 
 export function Routes() {
-  const user = useAuth()
+  const { user } = useAuth()
 
   return <BrowserRouter>{user ? <AppRoutes /> : <AuthRoutes />}</BrowserRouter>
 }
