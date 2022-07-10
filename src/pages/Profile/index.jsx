2cbad5d67ba5469c73d079532b0399f1771f2c37
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2022-07-10 08:40:43 -0300

    Carregando dados do usuário

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -4,8 +4,17 @@ import { Link } from 'react-router-dom'
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
+import { useState } from 'react'
+import { useAuth } from '../../hooks/auth'
 
 export function Profile() {
+  const { user } = useAuth()
+
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
+  const [passwordOld, setPasswordOld] = useState()
+  const [passwordNew, setPasswordNew] = useState()
+
   return (
     <Container>
       <header>
@@ -21,8 +30,20 @@ export function Profile() {
             <input id="avatar" type="file" />
           </label>
         </Avatar>
-        <Input placeholder="Nome" type="text" icon={FiUser} />
-        <Input placeholder="Email" type="text" icon={FiMail} />
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <Input
+          placeholder="Email"
+          type="text"
+          icon={FiMail}
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
         <Input placeholder="Senha atual" type="password" icon={FiLock} />
         <Input placeholder="Nova senha" type="password" icon={FiLock} />
 
