40a7d51a091a95e8534047de156908ebb867eec3
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2022-07-11 21:02:52 -0300

    Remover nota

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -19,6 +19,15 @@ export function Details() {
     navigate(-1)
   }
 
+  async function handleRemove() {
+    const confirm = window.confirm('Deseja realmente remover a nota?')
+
+    if (confirm) {
+      await api.delete(`/notes/${params.id}`)
+      handleBack()
+    }
+  }
+
   useEffect(() => {
     async function fetchNote() {
       const response = await api.get(`/notes/${params.id}`)
@@ -35,7 +44,7 @@ export function Details() {
       {data && (
         <main>
           <Content>
-            <ButtonText title="Excluir nota" />
+            <ButtonText onClick={handleRemove} title="Excluir nota" />
 
             <h1>{data.title}</h1>
 
